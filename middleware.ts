dadeb75c225c6526317683180a91f9fe9df7c0d6
LegspCpd <3572653147@qq.com>
2026-09-13 21:43:20 +0800

    Refactor authMiddleware publicRoutes configuration

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: (req) =>
-    !req.url.includes("/dashboard") && !req.url.includes("/code"),
-})
+  publicRoutes: ["/", "/api/lb-auth"],
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+};
