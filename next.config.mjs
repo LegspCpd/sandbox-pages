dc75a78a6a898de1e8a7e863aa495f89de806240
LegspCpd <3572653147@qq.com>
2026-09-13 21:13:48 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,12 @@ const nextConfig = {
   reactStrictMode: false,
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // 1. 让 Webpack 解析 monaco-editor 时直接指向纯 API 单文件，切断对 worker 文件的静态抓取
+      // 避免 Terser 抓取并压缩 monaco-editor 内部的 ESM Web Worker 造成语法错误
       config.resolve.alias = {
         ...config.resolve.alias,
         "monaco-editor$": "monaco-editor/esm/vs/editor/editor.api.js",
       };
 
-      // 2. 避免 node 模块在浏览器端报错
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
